6db0d42399b8ca808f6ff7b23221248012006e98
Nikolai <gardemarinin5@yandex.ru>
2023-11-27 12:24:27 +0500

    DP check2

--- ewm-stats-service/statistic-server/src/test/java/ru/practicum/server/repository/StatsRepositoryTest.java
+++ ewm-stats-service/statistic-server/src/test/java/ru/practicum/server/repository/StatsRepositoryTest.java
@@ -3,7 +3,6 @@ package ru.practicum.server.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.server.model.ViewStat;
 
 import java.time.LocalDateTime;
